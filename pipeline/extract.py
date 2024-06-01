40d7f583451760894593053ce677a2362fdea552
Jan Marko Mühlnikel <jan.muehlnikel@gmx.de>
2024-06-01 11:15:46 +0200

    removed standart inputs from ouput dataframes and just keep id and extracted values

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -10,19 +10,21 @@ from actions import SendFuelOrderAction
 from actions import SetActualBagWeightIndicatorAction
 from actions import StoreRegistrationAndConfigurationAc
 from actions import UpdateFuelDataAction
+from actions import EstimateStorePaxDataAction
 
 # Read the parquet files
 abc_file_path = os.path.join(os.path.dirname(__file__), '../data', 'ABCD_tripfiles.parquet')
 df_abcd = pd.read_parquet(abc_file_path, engine="pyarrow")
 
 df = pd.DataFrame()
-#CalculateWeightAndTrimAction.extract_abcd(df_abcd)
-#StorePaxDataAction.extract_abcd(df_abcd)
-#CreateZFWMessageAction.extract_abcd(df_abcd)
-#CreatLoadSheetAction.extract_abcd(df_abcd)
-#SendLoadsheetAction.extract_abcd(df_abcd)
-#CheckinMsgProcessor.extract_abcd(df_abcd)
-#SendFuelOrderAction.extract_abcd(df_abcd)
-#SetActualBagWeightIndicatorAction.extract_abcd(df_abcd)
-#StoreRegistrationAndConfigurationAc.extract_abcd(df_abcd)
+CalculateWeightAndTrimAction.extract_abcd(df_abcd)
+StorePaxDataAction.extract_abcd(df_abcd)
+CreateZFWMessageAction.extract_abcd(df_abcd)
+CreatLoadSheetAction.extract_abcd(df_abcd)
+SendLoadsheetAction.extract_abcd(df_abcd)
+CheckinMsgProcessor.extract_abcd(df_abcd)
+SendFuelOrderAction.extract_abcd(df_abcd)
+SetActualBagWeightIndicatorAction.extract_abcd(df_abcd)
+StoreRegistrationAndConfigurationAc.extract_abcd(df_abcd)
 UpdateFuelDataAction.extract_abcd(df_abcd)
+#EstimateStorePaxDataAction.extract_abcd(df_abcd)
