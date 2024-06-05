d6555be3f364e32fa0b32015e0efa97e6e6c071d
Jan Marko Mühlnikel <jan.muehlnikel@gmx.de>
2024-06-05 21:54:37 +0200

    updated SendLoadSheetAction

--- pipeline/actions/SendLoadsheetAction.py
+++ pipeline/actions/SendLoadsheetAction.py
@@ -1,43 +1,35 @@
 import re
-from tqdm import tqdm
-import pandas as pd
-
-from utils import random_sample
-from utils import add_to_df
+import json
+import utils
 
 ACTION = "SendLoadsheetAction"
 
-def extract_abcd(df):
-    action_df = df[df.action_name == ACTION]
-    output_df = action_df[["id"]].copy()
+def extract(message):
+    if "Message type        :   LOADSHEET" or "Message type        :   LOADING_INSTRUCTION" in message:
+        pattern_edno = r"(EDNO|Edition number) (\d+)"
+        pattern_takeoff_weight_actual = r'(TAKE OFF WEIGHT ACTUAL|TAKE OFF WEIGHT  ACTUAL)\s+(\d+)'  # Match one or more digits for takeoff weight
+        pattern_tow = r'\bTOW\b\s+(\d+)'
+        pattern_all_weights = r'ALL WEIGHTS IN KILOGRAM'  # Exact match for weights unit
 
-    for idx, row in tqdm(action_df.iterrows(), total=action_df.shape[0], desc="Processing rows"):
-        current_row = row["entry_details"]
+        # Extract information using regular expressions
+        edno_match = re.search(pattern_edno, message)
+        takeoff_weight_actual_match = re.search(pattern_takeoff_weight_actual, message)
+        tow_match = re.search(pattern_tow, message)
+        all_weights_match = re.search(pattern_all_weights, message)
 
-        if "Message type        :   LOADSHEET" or "Message type        :   LOADING_INSTRUCTION" in current_row:
-            pattern_edno = r"EDNO (\d+)"
-            pattern_takeoff_weight_actual = r'TAKE OFF WEIGHT ACTUAL\s+(\d+)'  # Match one or more digits for takeoff weight
-            pattern_all_weights = r'ALL WEIGHTS IN KILOGRAM'  # Exact match for weights unit
+        extracted_data = {}
+        extracted_data['EDNO'] = edno_match.group(1) if edno_match else None
+        extracted_data['TAKE_OFF_WEIGHT_ACTUAL'] = tow_match.group(1) if tow_match else (takeoff_weight_actual_match.group(1) if takeoff_weight_actual_match else None)
+        extracted_data['Weights_unit'] = 'KILOGRAM' if all_weights_match else None
 
-            # Extract information using regular expressions
-            edno_match = re.search(pattern_edno, current_row)
-            takeoff_weight_actual_match = re.search(pattern_takeoff_weight_actual, current_row)
-            all_weights_match = re.search(pattern_all_weights, current_row)
-
-            extracted_data = {}
-            extracted_data['Loadsheet_Version_Number'] = edno_match.group(1) if edno_match else None
-            extracted_data['TAKE_OFF_WEIGHT_ACTUAL'] = takeoff_weight_actual_match.group(1) if takeoff_weight_actual_match else None
-            extracted_data['Weights_unit'] = 'KILOGRAM' if all_weights_match else None
-
-            add_to_df(output_df, extracted_data, idx)
-        elif "STATUS LOADSHEET" in current_row:
+        if extracted_data["TAKE_OFF_WEIGHT_ACTUAL"] == None:
             pass
-        elif "STATUS LOADING_INSTRUCTION" in current_row:
-            pass
-        elif "com.onesystem.lc2.common.dto.SendDocumentDTO" in current_row:
-            pass
-        else:
-            print(current_row)
 
-    output_df.to_csv(f"pipeline/actions/actions_data/abcd_{ACTION}.csv")
-    return output_df
+        json.dumps(extracted_data)
+    elif "STATUS LOADSHEET" or "STATUS LOADING_INSTRUCTION" or "Email receivers     :" in message:
+        pass
+    elif "com.systemone.lc2.common.dto.SendDocumentDTO" or "com.onesystem.lc2.common.dto.SendDocumentDTO"  in  message:
+        pass
+    else:
+        print(message)
+        pass
