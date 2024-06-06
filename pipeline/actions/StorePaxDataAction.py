408503e73a32e24fccb957e6a5ac28f7431aa240
Jan Marko Mühlnikel <jan.muehlnikel@gmx.de>
2024-06-06 13:06:02 +0200

    updated StorePaxDataAction

--- pipeline/actions/StorePaxDataAction.py
+++ pipeline/actions/StorePaxDataAction.py
@@ -1,82 +1,96 @@
 import re
-from tqdm import tqdm
-
-from utils import random_sample
-from utils import add_to_df
+import json
+import utils
 
 ACTION = "StorePaxDataAction"
 
-def extract_abcd(df):
-    action_df = df[df.action_name == ACTION]
-    output_df = action_df[["id"]].copy()
+def extract(message):
 
-    for idx, row in tqdm(action_df.iterrows(), total=action_df.shape[0], desc="Processing rows"):
-        current_row = row["entry_details"]
-
-        # skip system row because of lacking information
-        if "com.onesystem.lc2.paxactuals.dto.PaxDataDTO" in current_row:
-            # pattern to extract infos from string
-            pattern = re.compile(
-            r'(?P<type>Checkin|Loadsheet)\s+'
-            r'(?P<Y>\d+|\w+)\s+'
-            r'(?P<Jump>\d+|\w+)\s+'
-            r'(?P<Standby>\d+|\w+)\s+'
-            r'(?P<Male>\d+|\w+)\s+'
-            r'(?P<Female>\d+|\w+)\s+'
-            r'(?P<Child>\d+|\w+)\s+'
-            r'(?P<Infant>\d+|\w+)\s+'
-            r'(?P<Bags>\d+|\w+)\s+'
-            r'(?P<BWgt>[0-9.]+|\w+)\s+'
-            #r'(?P<Average>[0-9.]+|\w+)'
+    # skip system row because of lacking information
+    if "Y              Jump           Standby        Male           Female         Child          Infant         Bags           BWgt           Average" or "                Y              J              Jump           Standby        Male           Female         Child          Infant         Bags           BWgt           Average        " in message:
+        pattern = re.compile(
+        r'(?P<type>\w+)\s+'
+        r'(?P<Y>\d+|NULL)\s+'
+        r'(?P<Jump>\d+|NULL)\s+'
+        r'(?P<Standby>\d+|NULL)\s+'
+        r'(?P<Male>\d+|NULL)\s+'
+        r'(?P<Female>\d+|NULL)\s+'
+        r'(?P<Child>\d+|NULL)\s+'
+        r'(?P<Infant>\d+|NULL)\s+'
+        r'(?P<Bags>\d+|NULL)\s+'
+        r'(?P<BWgt>[0-9.]+|NULL)\s*KG\s+'
+        r'(?P<Average>[0-9.]+|NULL)\s*'
         )
-            # find match from pattern
-            match = pattern.search(current_row)
-            
 
-            # add to teh data frame if match
-            if match:
-                extracted_data = match.groupdict()
-                add_to_df(output_df, extracted_data, idx)
 
-            else:
-                print("No match found")
-                print(current_row)
-                break
+        match = pattern.search(message)
 
-        elif "TOTAL Pax" in current_row:
-            pattern = re.compile(
-                r'TOTAL Pax:\s*(?P<TOTAL_Pax>\d+)\s*'
-                r'Y:\s*(?P<Y>\d+)\s*'
-                r'Jump:\s*(?P<Jump>\d+|NULL)\s*'
-                r'StandBy:\s*(?P<Standby>\d+|NULL)\s*'
-                r'Male:\s*(?P<Male>\d+|NULL)\s*'
-                r'Female:\s*(?P<Female>\d+|NULL)\s*'
-                r'Child:\s*(?P<Child>\d+|NULL)\s*'
-                r'Infant:\s*(?P<Infant>\d+|NULL)\s*'
-                r'Total bag:\s*(?P<Bags>\d+)\s*'
-                r'Total bag weight:\s*(?P<BWgt>[\d.]+ KG)\s*'
-                r'Baggage weight type:\s*(?P<Baggage_weight_type>\w+)'
-            )
-
-            # Extracting matches
-            match = pattern.search(current_row)
-
-            # Extracted key-value pairs
-            if match:
-                extracted_data = match.groupdict()
-                #print(extracted_data)
-                add_to_df(output_df, extracted_data, idx)
-            else:
-                print("----")
-                print("No match found")
-                print(current_row)
-                break
-                    
+        if match:
+            extracted_data = match.groupdict()
+            return json.dumps(extracted_data)
+        
         else:
-            print("unexpected format")
-            print(current_row)
-            break
+            #print("No match found.")
+            #utils.write_to_file(message, "sample.txt")
+            #raise NotImplemented
+            pass
 
+    elif "com.onesystem.lc2.paxactuals.dto.PaxDataDTO" in message:
 
-    output_df.to_csv(f"pipeline/actions/actions_data/abcd_{ACTION}.csv")
-    return output_df
+        # pattern to extract infos from string
+        pattern = re.compile(
+        r'(?P<type>Checkin|Loadsheet)\s+'
+        r'(?P<Y>\d+|\w+)\s+'
+        r'(?P<Jump>\d+|\w+)\s+'
+        r'(?P<Standby>\d+|\w+)\s+'
+        r'(?P<Male>\d+|\w+)\s+'
+        r'(?P<Female>\d+|\w+)\s+'
+        r'(?P<Child>\d+|\w+)\s+'
+        r'(?P<Infant>\d+|\w+)\s+'
+        r'(?P<Bags>\d+|\w+)\s+'
+        r'(?P<BWgt>[0-9.]+|\w+)\s+'
+        #r'(?P<Average>[0-9.]+|\w+)'
+    )
+        # find match from pattern
+        match = pattern.search(message)
+        
+
+        # add to teh data frame if match
+        if match:
+            extracted_data = match.groupdict()
+
+            return json.dumps(extracted_data)
+
+        else:
+            print("No match found")
+            print(message)
+            pass
+
+    elif "TOTAL Pax" in message:
+        pattern = re.compile(
+                r'(?:TOTAL Pax:\s*(?P<TOTAL_Pax>\w+)\s*)?'
+                r'(?:Y:\s*(?P<Y>\w+)\s*)?'
+                r'(?:Jump:\s*(?P<Jump>\w+|NULL)\s*)?'
+                r'(?:StandBy:\s*(?P<Standby>\w+|NULL)\s*)?'
+                r'(?:Male:\s*(?P<Male>\w+|NULL)\s*)?'
+                r'(?:Female:\s*(?P<Female>\w+|NULL)\s*)?'
+                r'(?:Child:\s*(?P<Child>\w+|NULL)\s*)?'
+                r'(?:Infant:\s*(?P<Infant>\w+|NULL)\s*)?'
+                r'(?:Total bag:\s*(?P<Bags>\w+)\s*)?'
+                r'(?:Total bag weight:\s*(?P<BWgt>[\d.]+ KG)\s*)?'
+                r'(?:Baggage weight type:\s*(?P<Baggage_weight_type>\w+))?.*'
+        )
+        match = pattern.search(message)
+
+        if match:
+            extracted_data = match.groupdict()
+            return json.dumps(extracted_data)
+        else:
+            print("No match found")
+            #utils.write_to_file(message, "sample.txt")
+            pass
+                
+    else:
+        print("Format not found!")
+        utils.write_to_file(message, "sample.txt")
+        pass
