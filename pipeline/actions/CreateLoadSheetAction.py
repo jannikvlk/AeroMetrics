5eff4bb9d87a2edf43408f70e97d93b5fec99969
Jannik Voelker <ampullen-raver0p@icloud.com>
2024-06-26 10:40:09 +0200

    fortschritt weights

--- pipeline/actions/CreateLoadSheetAction.py
+++ pipeline/actions/CreateLoadSheetAction.py
@@ -6,7 +6,6 @@ from actions.remove_typos import remove_typos
 def extract(message: str) -> str | None:
     message = remove_typos(message)
   
-
     if (
         "Message type        :   LOADSHEET" in message
         or "Message type        :   LOADING_INSTRUCTION" in message
@@ -19,12 +18,12 @@ def extract(message: str) -> str | None:
                 'TAKE OFF WEIGHT ACTUAL'
                 'TRIP'
                 'LANDING WEIGHT ACTUAL'
-                },"""
-        
+                },"""    
         
         patterns = {}
         if "TRIP FUEL" in message:
-            # This identifies zyxw messages because they are the only ones with TRIP followed by FUEL
+            # This identifies zyxw messages because they are the only ones 
+            # with TRIP followed by FUEL
             patterns = {
                 "TOTAL TRAFFIC LOAD": r"TOTAL\s+TRAFFIC\s+LOAD\s+(\d+)",
                 "DRY OPERATING WEIGHT": r"DRY\s+OPERATING\s+WEIGHT\s+(\d+)",
@@ -35,7 +34,8 @@ def extract(message: str) -> str | None:
                 "LANDING WEIGHT ACTUAL": r"LANDING\s+WEIGHT\s+ACTUAL\s+(\d+)",
             }
 
-        elif "ZERO FUEL WEIGHT ACTUAL" in message: # This identifies abcd messages
+        elif "ZERO FUEL WEIGHT ACTUAL" in message: 
+            # This identifies abcd messages
           
             patterns = {
                 "TOTAL TRAFFIC LOAD": r"TOTAL TRAFFIC LOAD\s+(\d+)",
@@ -47,29 +47,31 @@ def extract(message: str) -> str | None:
                 "LANDING WEIGHT ACTUAL": r"LANDING WEIGHT ACTUAL\s+(\d+)",
             }
         elif "ZFW" in message:
-            # This identifies mnop messages because they are the only ones with abbreviations like ZFW
+            # This identifies mnop messages because they are the only ones 
+            # with abbreviations like ZFW
             patterns = {
-            "DRY OPERATING WEIGHT": r"DOW\s+(\d+)", # DOW
-            "TOTAL TRAFFIC LOAD": r"PAY\s+(\d+)", # PAY
-            "ZERO FUEL WEIGHT ACTUAL": r"ZFW\s+(\d+)", # ZFW
-            "TAKE OFF FUEL": r"TOF\s+(\d+)", # TOF
-            "TAKE OFF WEIGHT ACTUAL": r"TOW\s+(\d+)", # TOW
-            "TRIP FUEL": r"TIF\s+(\d+)", # TIF
-            "LANDING WEIGHT ACTUAL": r"LAW\s+(\d+)" # LAW
+                "DRY OPERATING WEIGHT": r"DOW\s+(\d+)",  # DOW
+                "TOTAL TRAFFIC LOAD": r"PAY\s+(\d+)",  # PAY
+                "ZERO FUEL WEIGHT ACTUAL": r"ZFW\s+(\d+)",  # ZFW
+                "TAKE OFF FUEL": r"TOF\s+(\d+)",  # TOF
+                "TAKE OFF WEIGHT ACTUAL": r"TOW\s+(\d+)",  # TOW
+                "TRIP FUEL": r"TIF\s+(\d+)",  # TIF
+                "LANDING WEIGHT ACTUAL": r"LAW\s+(\d+)"  # LAW
             }
-        
+
         extracted_weights = {}
         for key, pattern in patterns.items():
             match = re.search(pattern, message)
             if match:
                 extracted_weights[key] = int(match.group(1))
             else:
-                extracted_weights[key] = None  # Assign None if the value is not found
-        
-        return json.dumps(extracted_weights)
-    
+                extracted_weights[key] = None
+                # Assign None if the value is not found
 
-    if "STATUS LOADSHEET" in message or "STATUS LOADING_INSTRUCTION" in message:
+        return json.dumps(extracted_weights)
+
+    if ("STATUS LOADSHEET" in message
+            or "STATUS LOADING_INSTRUCTION" in message):
         return None
     if "lc2.common.dto.SingleAttributeDTO" in message:
         return None
